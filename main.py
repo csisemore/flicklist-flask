bd2c51d2b66297f2a607a5fd8f58376c6ed3e696
csisemore <43246653+csisemore@users.noreply.github.com>
2018-10-29 20:47:51 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,60 +1,9 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import cgi
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-
-page_header = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>FlickList</title>
-    </head>
-    <body>
-        <h1>FlickList</h1>
-"""
-
-page_footer = """
-    </body>
-</html>
-"""
-
-# a form for adding new movies
-add_form = """
-    <form action="/add" method="post">
-        <label>
-            I want to add
-            <input type="text" name="new-movie"/>
-            to my watchlist.
-        </label>
-        <input type="submit" value="Add It"/>
-    </form>
-"""
-
-def get_current_watchlist():
-    # returns user's current watchlist--hard coded for now
-    return ['John Wick', 'John Wick Chapter 2', 'The Equalizer','The Equalizer 2', '31', 'House of 1000 Corpses', """The Devil's Rejects""", 'Deadpool', 'Deadpool 2']
-    # TODO: randomly choose one of the movies, and return it ]
-
-# a form for crossing off watched movies
-# (first we build a dropdown from the current watchlist items)
-crossoff_options = ""
-for movie in get_current_watchlist():
-    crossoff_options += '<option value="{0}">{0}</option>'.format(movie)
-
-crossoff_form = """
-    <form action="/crossoff" method="post">
-        <label>
-            I want to cross off
-            <select name="crossed-off-movie"/>
-                {0}
-            </select>
-            from my watchlist.
-        </label>
-        <input type="submit" value="Cross It Off"/>
-    </form>
-""".format(crossoff_options)
 
 # a list of movies that nobody should have to watch
 terrible_movies = [
@@ -65,6 +14,39 @@ terrible_movies = [
     "Starship Troopers"
 ]
 
+def get_current_watchlist():
+    # returns user's current watchlist--hard coded for now
+    return ['31', 'John Wick', 'The Equalizer', "'The Devil's Reject's", 'House of 1000 Corpses', 'John Wick Chapter 2', 'Deadpool', 'Deadpool 2']
+
+# TODO: 
+# Modify "My Watchlist" so that you eliminate the need for the "crossoff" form in edit.html. 
+# Now, next to every list item/movie listed in "My Watchlist" you should display a button that says "I Watched it!". 
+# Clicking the button will result in a confirmation message that the movie has been watched. 
+# So you'll need to add a form within the <li> tags of "My Watchlist"
+# Once this is done, delete the "crossoff" form in edit.html
+
+# TODO:
+# Make a ratings.html template which lists all movies that have been crossed off.
+# It should have a header of <h2>Movies I Have Watched</h2>
+# Add a form for rating EACH list item/movie using a <select> dropdown with the options/values
+# in this list: ["How was it?", "*", "**", "***", "****", "*****"]
+# And with a button that says "Rate It!" to submit the user's rating.
+# Give this form the action of "/rating-confirmation" and the method of post.
+
+# TODO: 
+# Add a function, movie_ratings, to handle a get request and render the template at "/ratings"
+
+# TODO:
+# Add a function, get_watched_movies, to get the list of crossed off movies. 
+# For now, create a hard-coded list with a few movie titles. 
+
+# TODO:
+# Make a rating-confirmation.html template, to be displayed when the user rates a movie 
+# they have crossed off. 
+
+# TODO: 
+# create a rate_movie function that handles a post request on /rating-confirmation and 
+# renders the `rating-confirmation` template.
 
 @app.route("/crossoff", methods=['POST'])
 def crossoff_movie():
@@ -79,54 +61,33 @@ def crossoff_movie():
         return redirect("/?error=" + error)
 
     # if we didn't redirect by now, then all is well
-    crossed_off_movie_element = "<strike>" + crossed_off_movie + "</strike>"
-    confirmation = crossed_off_movie_element + " has been crossed off your Watchlist."
-    content = page_header + "<p>" + confirmation + "</p>" + page_footer
-
-    return content
-
+    return render_template('crossoff.html', crossed_off_movie=crossed_off_movie)
 
 @app.route("/add", methods=['POST'])
 def add_movie():
+    # look inside the request to figure out what the user typed
     new_movie = request.form['new-movie']
 
-    # TODO 
-    # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-    
-    # TODO 
     # if the user typed nothing at all, redirect and tell them the error
+    if (not new_movie) or (new_movie.strip() == ""):
+        error = "Please specify the movie you want to add."
+        return redirect("/?error=" + error)
 
-    # TODO 
-    # if the user wants to add a terrible movie, redirect and tell them not to add it b/c it sucks
+    # if the user wants to add a terrible movie, redirect and tell them the error
+    if new_movie in terrible_movies:
+        error = "Trust me, you don't want to add '{0}' to your Watchlist".format(new_movie)
+        return redirect("/?error=" + error)
 
-    # build response content
-    new_movie_element = "<strong>" + new_movie + "</strong>"
-    sentence = new_movie_element + " has been added to your Watchlist!"
-    content = page_header + "<p>" + sentence + "</p>" + page_footer
+    # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+    new_movie_escaped = cgi.escape(new_movie, quote=True)
 
-    return content
+    return render_template('add-confirmation.html', movie=new_movie)
 
 
 @app.route("/")
 def index():
-    edit_header = "<h2>Edit My Watchlist</h2>"
-
-    # if we have an error, make a <p> to display it
-    error = request.args.get("error")
-    if error:
-        error_esc = cgi.escape(error, quote=True)
-        error_element = '<p class="error">' + error_esc + '</p>'
-    else:
-        error_element = ''
-
-    # combine all the pieces to build the content of our response
-    main_content = edit_header + add_form + crossoff_form + error_element
-
-
-    # build the response string
-    content = page_header + main_content + page_footer
-
-    return content
-
+    encoded_error = request.args.get("error")
+    return render_template('edit.html', watchlist=get_current_watchlist(), error=encoded_error and cgi.escape(encoded_error, quote=True))
 
 app.run()
+
